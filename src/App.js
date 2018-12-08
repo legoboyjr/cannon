aaebcaf88822f9a70bcfee668f7097db4ba86526
Alexander Thompson <legoboyjr@gmail.com>
2018-12-08 14:28:02 -0500

    log in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getCanvasPosition } from './utils/formulas';
 import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'legoboyjr.auth0.com',
+  clientID: 'fgCoS3EnFqFahgakddOTPBjAqYnr9dRs',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
 
 class App extends Component {
   componentDidMount() {
@@ -10,6 +19,10 @@ class App extends Component {
       const cnv = document.getElementById('aliens-go-home-canvas');
       cnv.style.width = `${window.innerWidth}px`;
       cnv.style.height = `${window.innerHeight}px`;
+      Auth0.handleAuthCallback();
+      Auth0.subscribe((auth) => {
+        console.log(auth);
+      });
     };
     window.onresize();
     setInterval(() => {
@@ -24,8 +37,10 @@ class App extends Component {
   render() {
     return (
       <Canvas
-        angle={this.props.angle}
-        trackMouse={event => (this.trackMouse(event))}
+      angle={this.props.angle}
+      gameState={this.props.gameState}
+      startGame={this.props.startGame}
+      trackMouse={event => (this.trackMouse(event))}
       />
     );
   }
@@ -33,7 +48,20 @@ class App extends Component {
 
 App.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
   moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default App;
--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,12 +4,12 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonBase from './CannonBase';
 import CannonPipe from './CannonPipe';
-import CannonBall from './CannonBall';
-import CurrentScore from './CurrentScore';
+import CurrentScore from './CurrentScore'
 import FlyingObject from './FlyingObject';
-import Heart from './Heart';
 import StartGame from './StartGame';
 import Title from './Title';
+import Login from './Login';
+import { signIn } from 'auth0-web';
 
 const Canvas = (props) => {
   const gameHeight = 1200;
@@ -28,24 +28,46 @@ const Canvas = (props) => {
       </defs>
       <Sky />
       <Ground />
-      <Title />
       <CannonPipe rotation={props.angle} />
       <CannonBase />
-      <CannonBall position={{ x: 0, y: -100 }} />
-      <StartGame onClick={() => console.log('Aliens, Go Home!')} />
       <CurrentScore score={15} />
-      <Heart position={{ x: -300, y: 35 }} />
-      <FlyingObject position={{ x: -150, y: -300 }} />
-      <FlyingObject position={{ x: 150, y: -300 }} />
+
+      {!props.gameState.started &&
+        <g>
+          <StartGame onClick={() => props.startGame()} />
+          <Title />
+          <Login authenticate={signIn} /> 
+        </g>
+      }
+
+      {props.gameState.flyingObjects.map(flyingObject => (
+        <FlyingObject
+          key={flyingObject.id}
+          position={flyingObject.position}
+        />
+      ))}
     </svg>
   );
 };
 
+
+
 Canvas.propTypes = {
   angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
   trackMouse: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
-
-
 
 export default Canvas;
